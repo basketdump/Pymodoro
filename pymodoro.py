e86178d2351701ab29a4e0996af297ceb4dbe059
basketdump <tylerisop@gmail.com>
2018-03-05 21:29:12 -0600

    First push

---
+++ pymodoro.py
@@ -0,0 +1,67 @@
+'''Python implementation of the Pomodoro Technique'''
+import time
+import notify2
+
+
+def notify(activity, end):
+    '''This method sends a desktop notification of current activity'''
+    activity_message = "Your " + activity + " has started."
+    end_message = "It will end at " + end
+    notify2.Notification(activity_message, end_message).show() 
+    return
+
+
+def time_estimate(length):
+    '''this method returns time now + length in minutes'''
+    current_time = time.time() # time in seconds since epoch
+    estimate_time = current_time + (length) # current_time + length(mins)
+    return time.ctime(estimate_time)
+
+
+def break_session(break_length):
+    '''This method implements the small or long break sessions'''
+    end = time_estimate(break_length)
+    print("BREAK STARTED. Work resumes at", end)
+    notify("break", end)
+    # pause for break_length minutes
+    time.sleep(break_length)
+    return
+
+
+def work_session(work_length):
+    '''This method implements the work session'''
+    end = time_estimate(work_length)
+    print("WORK STARTED. Next break is at", end)
+    notify("work", end)
+    # pause for work_length minutes
+    time.sleep(work_length)
+    return
+
+
+def main():
+    '''this is our main method to call and manage arguments to work and break
+       and handle initial input'''
+    
+    # initialize notify2
+    notify2.init('Pymodoro')
+
+    work_length = int(input("Enter work interval (minutes): ")) * 60
+    break_length = int(input("Enter short break interval (minutes): ")) * 60
+    work_count = 0
+
+    while (True):
+        work_session(work_length)
+        work_count += 1
+        
+        # is it time for a long break?
+        if work_count % 4 == 0:
+            break_session(break_length * 3) 
+        
+        else:
+            break_session(break_length)
+
+    return
+
+
+main()
+
